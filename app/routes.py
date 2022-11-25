6c2155c283f73e6314dc686b0b23920acbd8403e
Maester of Bots <111409615+maester-of-bots@users.noreply.github.com>
2022-11-24 20:49:39 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -9,16 +9,15 @@ import geocoder
 def index():
 
     # if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-    data1 = (request.environ['REMOTE_ADDR'])
+    data = (request.environ['REMOTE_ADDR'])
 
-    data2 = (request.environ)  # if behind a proxy
+    # data2 = (request.environ)  # if behind a proxy
 
-    data3 = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    # data3 = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
 
-    loc_data = geocoder.ip(data1).json
+    loc_data = geocoder.ip(data).json
     if 'hostname' in loc_data.keys():
         hostname = loc_data['hostname']
     else:
         hostname = ''
-    # return render_template('fuckingip.html', ip=loc_data['ip'],address=loc_data['address'],hostname=hostname,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url="https://ipinfo.io/"+loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
-    return render_template('fuckingip.html', ip=data1,address=data3,hostname=data3,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url="https://ipinfo.io/"+loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
+    return render_template('fuckingip.html', ip=loc_data['ip'],address=loc_data['address'],hostname=hostname,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url="https://ipinfo.io/"+loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
