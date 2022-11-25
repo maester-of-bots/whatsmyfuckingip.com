02ec38ad0209442b476a1d54ecae755ab26af9f8
Maester of Bots <111409615+maester-of-bots@users.noreply.github.com>
2022-11-24 20:45:25 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -14,10 +14,11 @@ def index():
     data2 = (request.environ)  # if behind a proxy
 
     data3 = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+
     loc_data = geocoder.ip(data1).json
     if 'hostname' in loc_data.keys():
         hostname = loc_data['hostname']
     else:
         hostname = ''
     # return render_template('fuckingip.html', ip=loc_data['ip'],address=loc_data['address'],hostname=hostname,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url="https://ipinfo.io/"+loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
-    return render_template('fuckingip.html', ip=data1,address=data2,hostname=data3,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url="https://ipinfo.io/"+loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
+    return render_template('fuckingip.html', ip=data1,address=data3,hostname=data3,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url="https://ipinfo.io/"+loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
