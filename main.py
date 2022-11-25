f28c7b2e255cfc5a48ee787665f67bf81614233c
Maester of Bots <111409615+maester-of-bots@users.noreply.github.com>
2022-11-24 20:21:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+# https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-centos-8
 from app import app
 
+if __name__ == "__main__":
+    app.run()
 
-app.run(host="0.0.0.0")
+
+
+
+
