8837575bee1e601a5ebe1d4831075f36987b9af9
Maester of Bots <111409615+maester-of-bots@users.noreply.github.com>
2022-11-26 17:18:24 -0500

    3

--- app/routes.py
+++ app/routes.py
@@ -12,10 +12,14 @@ def index():
     else:
         data = request.environ['HTTP_X_FORWARDED_FOR']
 
-
-    loc_data = geocoder.ip(data).json
-    if 'hostname' in loc_data.keys():
-        hostname = loc_data['hostname']
+    if data == '127.0.0.1':
+        return render_template('fuckingip.html', ip='127.0.0.1', address='Your fucking computer', hostname='Your fucking hostname.',
+                               lat='idfk', lon='idfc', isp='lol',
+                               isp_url="127.0.0.1", zipcode="eat it")
     else:
-        hostname = ''
-    return render_template('fuckingip.html', ip=loc_data['ip'],address=loc_data['address'],hostname=hostname,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url="https://ipinfo.io/"+loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
+        loc_data = geocoder.ip(data).json
+        if 'hostname' in loc_data.keys():
+            hostname = loc_data['hostname']
+        else:
+            hostname = ''
+        return render_template('fuckingip.html', ip=loc_data['ip'],address=loc_data['address'],hostname=hostname,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url="https://ipinfo.io/"+loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
