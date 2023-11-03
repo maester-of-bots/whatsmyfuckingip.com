b43ddfe6010a16f3a142cf36cc0112ab9695d41b
maester-of-bots <111409615+maester-of-bots@users.noreply.github.com>
2023-11-02 20:44:07 -0400

    Fucking ads

--- app/static/ads_2.js
+++
@@ -1,1 +0,0 @@
-(function(dcb){var d = document,s = d.createElement('script'),l = d.scripts[d.scripts.length - 1];s.settings = dcb || {};s.src = "\/\/pleasedexample.com\/bCX.Vts\/dvGclH0fYTWncI\/xe\/mF9NuDZ\/U\/lJknP\/TWQR2\/N_DPkw1eMOTbMatsNaDHY\/0lO_TbUVx-N\/As";l.parentNode.insertBefore(s, l);})({})
